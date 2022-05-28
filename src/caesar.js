c3344afa1a8c8915a7f1f378f2d458003060cf75
jesusthedev <104586580+jesusthedev@users.noreply.github.com>
2022-05-28 12:21:00 -0500

    updated src

--- src/caesar.js
+++ src/caesar.js
@@ -5,11 +5,47 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
   function caesar(input, shift, encode = true) {
-    // your solution code here
-  }
-
+    if (!shift || shift === 0 || shift < -25 || shift > 25) { return false }; // if shift outside of parameters return false
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // create a alphabet variable
+    const result = [];  // create result array variable
+    input = input.toLowerCase().split('');  // take input, make lowercase, split it into an array
+    
+    if (encode === true) { // if enocde === true 
+      input.forEach((char, index) => { 	// loop through  input
+        if (alphabet.includes(char)) { 	// make sure it is included in the alphabet variable 
+          alphabet.find((letter, index) => {
+            if (char === letter) {
+              let newIndex = index + shift; 	// find index add the shift
+              if( newIndex < 0) { newIndex = newIndex + 26}; 	// if shift is above or below 26 add 26 or minus 26 to get to other end of the array
+              if( newIndex > 25) { newIndex = newIndex - 26};
+              newChar = alphabet[newIndex];
+              result.push(newChar);
+            }
+          })
+        } else {
+          result.push(char); 	// push new alphabet index into result
+        }
+      });
+    } else {
+      input.forEach((char, index) => { 	// same as above but change the shift 
+        if (alphabet.includes(char)) {
+          alphabet.find((letter, index) => {
+            if (char === letter) {
+              let newIndex = index - shift;
+              if( newIndex < 0) { newIndex = newIndex + 26};
+              if( newIndex > 25) { newIndex = newIndex - 26};
+              newChar = alphabet[newIndex];
+              result.push(newChar);
+            }
+          })
+        } else {
+          result.push(char);
+        }
+      });
+    }
+    return result.join(''); //return array joined into string
+    }
   return {
     caesar,
   };
--- src/polybius.js
+++ src/polybius.js
@@ -4,10 +4,45 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
-
+  
   function polybius(input, encode = true) {
-    // your solution code here
+    //create polybius square collection
+    const polybiusSquare = {
+      1: { 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'},
+      2: { 1: 'f', 2: 'g', 3: 'h', 4: 'i/j', 5: 'k'},
+      3: { 1: 'l', 2: 'm', 3: 'n', 4: 'o', 5: 'p'},
+      4: { 1: 'q', 2: 'r', 3: 's', 4: 't', 5: 'u'},
+      5: { 1: 'v', 2: 'w', 3: 'x', 4: 'y', 5: 'z'},
+    };
+
+    const message = input.toLowerCase().split(''); //our input -> individual array
+    const messageNoSpaces = message.filter(nums => nums != ' '); //array  without spaces
+    if(!encode){ //then decode
+      let decodeString = '';
+      if(messageNoSpaces.length % 2 != 0) return false; // copy of array without spaces to check if there are an odd number
+      for(let index = 0; index < message.length; index += 2){
+        //[column][row] format
+        if(message[index] === ' '){ //if a space in the input add it to our decodeString
+          decodeString += ' ';
+          index--; //reset index a space since our value was a space
+        }else{ decodeString += polybiusSquare[message[index + 1]][message[index]]; } //decode
+      }
+      return decodeString;
+    }else{ //else we encode
+      const buildEncryption = [];
+      for(let letter of message){ //for each letter we will find the key/value pair by:
+        if(letter === ' '){ buildEncryption.push(' '); } //keep our spaces
+        for(let c = 1; c < 6; c++){ //cycling each column
+          for(let i = 1; i < 6; i++){ //cycling each row
+            if(polybiusSquare[c][i].includes(letter)){
+              buildEncryption.push(i);
+              buildEncryption.push(c);
+            } 
+          }
+        }
+      }
+      return buildEncryption.join('');
+    }
   }
 
   return {
